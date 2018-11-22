ea16d00876cff36c5ce2b116c23b3e2a22aee524
masterplanx <ferreyrasergio@gmail.com>
2018-11-22 18:14:47 -0300

    Testing

--- packer/private_instance/scripts/install-software.sh
+++ packer/private_instance/scripts/install-software.sh
@@ -4,4 +4,3 @@ sudo apt update
 #sudo apt -y upgrade
 sudo apt install -y build-essential python-dev python python-software-properties python-pip
 sudo pip install --upgrade pip
-sudo apt-get install -y nginx
